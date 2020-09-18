439b7dc5ca84ab2dbabd51ba847a886c4fdd58df
Stefan Moonen <stefan.moonen@gmail.com>
2020-09-18 15:24:39 +0200

    Initialize basicAuth property correctly

--- src/main/php/Handler/LokiHandler.php
+++ src/main/php/Handler/LokiHandler.php
@@ -13,8 +13,9 @@ namespace Itspire\MonologLoki\Handler;
 use Itspire\MonologLoki\Formatter\LokiFormatter;
 use Monolog\Formatter\FormatterInterface;
 use Monolog\Handler\AbstractProcessingHandler;
-use Monolog\Handler\Curl;
 use Monolog\Logger;
+use Symfony\Component\HttpClient\HttpClient;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class LokiHandler extends AbstractProcessingHandler
 {
@@ -22,7 +23,7 @@ class LokiHandler extends AbstractProcessingHandler
     protected ?string $entrypoint;
 
     /** the identifiers for Basic Authentication to the Loki system */
-    protected array $basicAuth = [];
+    protected ?array $basicAuth;
 
     /** the name of the system (hostname) sending log messages to Loki */
     protected ?string $systemName;
@@ -33,19 +34,39 @@ class LokiHandler extends AbstractProcessingHandler
     /** the list of default labels to be sent to the Loki system */
     protected array $globalLabels = [];
 
-    public function __construct(array $apiConfig, $level = Logger::DEBUG, $bubble = true)
+    protected HttpClientInterface $client;
+    protected \SplObjectStorage $responses;
+
+
+    public function __construct(array $apiConfig, int $level = Logger::DEBUG, bool $bubble = true, HttpClientInterface $client = null)
     {
         if (!function_exists('json_encode')) {
-            throw new \RuntimeException('PHP\'s json extension is required to use Monolog\'s LokiHandler');
+            throw new \RuntimeException(sprintf('The %s handler needs the PHP json extension.', __CLASS__));
         }
+
+        if (!interface_exists('\Symfony\Contracts\HttpClient\HttpClientInterface')) {
+            throw new \LogicException(sprintf('The %s handler needs an HTTP client. Try running "composer require symfony/http-client".', __CLASS__));
+        }
+
         parent::__construct($level, $bubble);
+
         $this->entrypoint = $this->getEntrypoint($apiConfig['entrypoint']);
         $this->globalContext = $apiConfig['context'] ?? [];
         $this->globalLabels = $apiConfig['labels'] ?? [];
         $this->systemName = $apiConfig['client_name'] ?? null;
-        if (isset($apiConfig['auth']['basic'])) {
-            $this->basicAuth = (2 === count($apiConfig['auth']['basic'])) ? $apiConfig['auth']['basic'] : [];
-        }
+        $this->basicAuth = (isset($apiConfig['auth']['basic']) && 2 === count($apiConfig['auth']['basic'])) ? $apiConfig['auth']['basic'] : null;
+        $this->client = $client ?: HttpClient::create(['timeout' => 1]);
+        $this->responses = new \SplObjectStorage();
+    }
+
+    public function __destruct()
+    {
+        $this->wait(true);
+    }
+
+    protected function getDefaultFormatter(): FormatterInterface
+    {
+        return new LokiFormatter($this->globalLabels, $this->globalContext, $this->systemName);
     }
 
     private function getEntrypoint(string $entrypoint): string
@@ -60,12 +81,10 @@ class LokiHandler extends AbstractProcessingHandler
     /** @throws \JsonException */
     public function handleBatch(array $records): void
     {
+        $records = array_filter($records, [$this, 'isHandling']);
         $rows = [];
-        foreach ($records as $record) {
-            if (!$this->isHandling($record)) {
-                continue;
-            }
 
+        foreach ($records as $record) {
             $record = $this->processRecord($record);
             $rows[] = $this->getFormatter()->format($record);
         }
@@ -77,35 +96,45 @@ class LokiHandler extends AbstractProcessingHandler
     private function sendPacket(array $packet): void
     {
         $payload = json_encode($packet, JSON_THROW_ON_ERROR | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+
         $url = sprintf('%s/loki/api/v1/push', $this->entrypoint);
-        $ch = curl_init($url);
-        if (!empty($this->basicAuth)) {
-            curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-            curl_setopt($ch, CURLOPT_USERPWD, implode(':', $this->basicAuth));
-        }
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt(
-            $ch,
-            CURLOPT_HTTPHEADER,
-            [
-                'Content-Type: application/json',
-                'Content-Length: ' . strlen($payload),
+        $response = $this->client->request('POST', $url, [
+            'auth_basic' => $this->basicAuth,
+            'body' => $payload,
+            'headers' => [
+                'Content-Type' => 'application/json',
+                'Content-Length' => strlen($payload)
             ]
-        );
+        ]);
 
-        Curl\Util::execute($ch);
-    }
+        $this->responses->attach($response);
 
-    protected function getDefaultFormatter(): FormatterInterface
-    {
-        return new LokiFormatter($this->globalLabels, $this->globalContext, $this->systemName);
+        $this->wait(false);
     }
 
     /** @throws \JsonException */
     protected function write(array $record): void
     {
         $this->sendPacket(['streams' => [$record['formatted']]]);
+    }
+
+    private function wait(bool $blocking)
+    {
+        foreach ($this->client->stream($this->responses, $blocking ? null : 0.0) as $response => $chunk) {
+            try {
+                if ($chunk->isTimeout() && !$blocking) {
+                    continue;
+                }
+                if (!$chunk->isFirst() && !$chunk->isLast()) {
+                    continue;
+                }
+                if ($chunk->isLast()) {
+                    $this->responses->detach($response);
+                }
+            } catch (ExceptionInterface $e) {
+                $this->responses->detach($response);
+                error_log(sprintf("Could not push logs to Loki:\n%s", (string) $e));
+            }
+        }
     }
 }
